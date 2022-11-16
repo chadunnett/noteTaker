c5e62d55bff1003a1c796599837b0783ab7ea7a4
chadunnett <chadunnett@gmail.com>
2022-11-15 18:31:46 -0600

    commit

--- Develop/routes/apiRoutes/index.js
+++ Develop/routes/apiRoutes/index.js
@@ -4,32 +4,46 @@ const  notes  = require('../../db/db.json')
 const fs = require('fs')
 
 
-router.get('/api/notes', (req, res) => {
- 
-    fs.readFile('../../db/db.json', "utf8", function(err, results){
-      console.log(results)
-    })
+
+router.get('/notes', (req, res) => {
+//  console.log(res)
+//     fs.readFile('../../db/db.json', "utf8", function(err, result){
+// res.json(result)
+//       console.log(result)
+      
+//     })
+return res.json(notes)
+
   });
 
-  router.post('/api/notes', (req, res) => {
+  router.post('/notes', (req, res) => {
     console.info(`${req.method} request received to add a note`);
     console.log(req.body);
+    fs.readFile('/db/db.json', 'utf8', (err, data) => {
+      if (err) {
+        console.error(err);
+      } else {
+         const parsedData = JSON.parse(data);
+         console.log(parsedData)
+        // parsedData.push(content);
+        // writeToFile(notes, parsedData);
+      }
+    })
+    // const { title, text, id } = req.body;
   
-    const { title, text, id } = req.body;
-  
-    if (req.body) {
-      const newNote = {
-        title,
-        text,
-        id
+    // if (req.body) {
+    //   const newNote = {
+    //     title,
+    //     text,
+    //     id
        
-      };
+    //   };
   
-      readAndAppend(newNote, '../../db/db.json');
-      res.json(`Note added successfully 🚀`);
-    } else {
-      res.error('Error in adding note');
-    }
+    //   readAndAppend(newNote, '../../db/db.json');
+    //   res.json(`Note added successfully 🚀`);
+    // } else {
+    //   res.error('Error in adding note');
+    // }
   });
 
 
--- Develop/server.js
+++ Develop/server.js
@@ -1,9 +1,8 @@
 const express = require('express');
-
-const PORT = process.env.PORT || 3001;
-const app = express();
 const apiRoutes = require('./routes/apiRoutes');
 const htmlRoutes = require('./routes/htmlRoutes');
+const PORT = process.env.PORT || 3001;
+const app = express();
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
